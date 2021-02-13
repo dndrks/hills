349e59c3c44e4c86b2bb4e812dece82f27278556
dndrks <derks.dan@gmail.com>
2021-02-13 12:00:12 -0500

    adjustable min/max window, 8 hills

--- hills.lua
+++ hills.lua
@@ -11,10 +11,12 @@ function init()
   midi_notes = {70,74,63,65,67,62,72,70,60,55,77,70,74,75,63,67}
   hills = {}
   for i = 1,2 do
-    hills[i] = {{},{},{},{}}
-    hills[i].segment = 1
+    hills[i] = {{},{},{},{},{},{},{},{}}
     hills[i].active = false
-    for j = 1,4 do
+    hills[i].min = 1
+    hills[i].max = #hills[i]
+    hills[i].segment = hills[i].min
+    for j = 1,#hills[i] do
       hills[i][j].step = 0
       hills[i][j].duration = util.round(clock.get_beat_sec() * math.random(4),0.01) -- in seconds
       -- hills[i][j].base_step = math.random(0,util.round(hills[i][j].duration*1000-1)) / 1000
@@ -61,10 +63,10 @@ iterate = function(i)
   if util.round(seg.step,0.01) == util.round(seg.duration,0.01) then
     seg.counter:stop()
     print("stopping", seg.counter.is_running)
-    if h.segment < 4 then
+    if h.segment < h.max then
       h.segment = h.segment + 1
       toggle_iterator(false,i)
-    elseif h.segment == 4 then
+    elseif h.segment == h.max then
       h.active = false
     end
     -- toggle_iterator(false,i)
@@ -90,11 +92,11 @@ end
 key = function(n,z)
   if n == 3 and z == 1 then
     hills[1][hills[1].segment].counter:stop()
-    hills[1].segment = 1
+    hills[1].segment = hills[1].min
     toggle_iterator(hills[1][1].counter.is_running,1)
   elseif n == 2 and z == 1 then
     hills[2][hills[2].segment].counter:stop()
-    hills[2].segment = 1
+    hills[2].segment = hills[2].min
     toggle_iterator(hills[2][1].counter.is_running,2)
   end
 end
